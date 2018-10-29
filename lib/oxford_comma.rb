70d59ccb46c3d846b19650819ebeb178746db22e
Richard Yance <yance0022@gmail.com>
2018-10-29 03:04:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,7 @@
 def oxford_comma(array)
-
+  if array.length == 2
+    return array.join(" and ")
+  elsif array.length > 2
+    return array[0...-2].join(" , ") , array[-2..-1].join(" and ")
+end
 end
