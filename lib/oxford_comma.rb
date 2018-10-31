54d2327ca3f459202c20ef0ea534129d8dee46ff
Richard Yance <yance0022@gmail.com>
2018-10-31 00:54:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length > 2
     return array[0...-1].join(", ") + " and #{array[-1]}"
   elsif array.length == 1
-    return "#{array[1]}"
+    return "#{array[0]}"
 
 end
 end
