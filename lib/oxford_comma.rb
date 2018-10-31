6666241006819854ac5fd20dedcc161092018415
Richard Yance <yance0022@gmail.com>
2018-10-31 00:54:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
   elsif array.length > 2
-    return array[0...-1].join(", ") + " and #{array[-1]}"
+    return array[0...-1].join(", ") + ", and #{array[-1]}"
   elsif array.length == 1
     return "#{array[0]}"
 
