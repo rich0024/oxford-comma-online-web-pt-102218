b96c1101b139374312e1e05977a5c3d6a03f15d4
Richard Yance <yance0022@gmail.com>
2018-10-31 00:46:22 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     return array.join(" and ")
   elsif array.length > 2
-    return array[0...-2].join(" , ") , array[-2..-1].join(" and ")
+    return array[0...-1].join(", ") + " and #{array[-1]}"  
 end
 end
+
